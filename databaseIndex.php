675f56b04c81e073a887216af3cfd28a699aa2e8
genericallyTerrible <jmoehrnkel@gmail.com>
2015-04-11 15:52:15 -0400

    Small iFrame script changes

--- databaseIndex.php
+++ databaseIndex.php
@@ -16,7 +16,7 @@ $content = [
                 function resizeIframe(obj) {
                     obj.style.height = 0;
                     obj.style.height = obj.contentWindow.document.body.scrollHeight + 2 + "px";
-                    if(obj.style.height == 8 + "px" || ((obj.style.height <= 9 + "px") && (obj.style.height >= 7 + "px" ))){
+                    if(obj.style.height == 10 + "px" || ((obj.style.height <= 11 + "px") && (obj.style.height >= 9 + "px" ))){
                         WarFrame.location.href = "php/database/ViewTable.php";
                     }
                     window.scrollTo(0,document.body.scrollHeight);
